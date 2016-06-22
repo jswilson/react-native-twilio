4506ed5169f834d62b7821a342acdf5a533e8240
Jonathan Wilson <js.wilson@ymail.com>
2016-06-21 18:17:39 -0600

    Update for accepting calls and compile fixes

--- android/src/main/java/com/rogchap/react/modules/twilio/TwilioModule.java
+++ android/src/main/java/com/rogchap/react/modules/twilio/TwilioModule.java
@@ -4,12 +4,16 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.modules.core.RCTNativeAppEventEmitter;
 
 import com.twilio.client.Device;
 import com.twilio.client.Connection;
 import com.twilio.client.ConnectionListener;
+import com.twilio.client.DeviceListener;
 import com.twilio.client.Twilio;
+import com.twilio.client.PresenceEvent;
 
 import java.net.URL;
 import java.net.URLConnection;
@@ -19,22 +23,63 @@ import java.io.BufferedReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.app.PendingIntent;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
-public class TwilioModule extends ReactContextBaseJavaModule implements ConnectionListener {
+public class TwilioModule extends ReactContextBaseJavaModule implements ConnectionListener, DeviceListener {
 
   private ReactContext _reactContext;
   private Device _phone;
   private Connection _connection;
   private Connection _pendingConnection;
+  private IntentReceiver _receiver;
+
+  public class IntentReceiver extends BroadcastReceiver {
+    private ConnectionListener _cl;
+
+    public IntentReceiver(ConnectionListener connectionListener) {
+        this._cl = connectionListener;
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        _pendingConnection = (Connection)intent.getParcelableExtra("com.twilio.client.Connection");
+        _pendingConnection.setConnectionListener(this._cl);
+        _pendingConnection.accept();
+        _connection = _pendingConnection;
+        _pendingConnection = null;
+        sendEvent("deviceDidReceiveIncoming", null);
+    }
+  }
 
   public TwilioModule(ReactApplicationContext reactContext) {
     super(reactContext);
+
     _reactContext = reactContext;
+    this._reactContext = reactContext;
+    this._receiver = new IntentReceiver(this);
+    IntentFilter intentFilter = new IntentFilter();
+    intentFilter.addAction("com.rogchap.react.modules.twilio.incoming");
+    this._reactContext.registerReceiver(this._receiver, intentFilter);
   }
 
-  private void sendEvent(String eventName, @Nullable WritableMap params) {
-    _reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-      .emit(eventName, params);
+  private void sendEvent(String eventName, @Nullable Map<String, String> params) {
+
+    if (eventName.equals("connectionDidDisconnect")) {
+      Log.e("mytag", "not emitting an event, just dereferncing the DeviceEventEmitter");
+      _reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).toString();
+      Log.e("mytag", "DONE");
+    }
+    else {
+      _reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit(eventName, null);
+    }
   }
 
   @Override
@@ -48,34 +93,38 @@ public class TwilioModule extends ReactContextBaseJavaModule implements Connecti
     try {
       URLConnection conn = new URL(tokenUrl).openConnection();
       InputStream in = conn.getInputStream();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+      BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+      String line = "";
       while ((line = reader.readLine()) != null) {
         sb.append(line);
       }
     } catch (Exception e) {
-      Log.e(e.getMessage(), e);
     }
     initWithToken(sb.toString());
   }
 
   @ReactMethod
-  public void initWithToken(String token) {
+  public void initWithToken(final String token) {
+    final DeviceListener dl = this;
+
     if (!Twilio.isInitialized()) {
-      Twilio.initialize(getApplicationContext(), new Twilio.InitListener() {
+      Twilio.initialize(_reactContext, new Twilio.InitListener() {
         @Override
           public void onInitialized() {
             try {
               if (_phone == null) {
-                _phone = Twilio.createDevice(capabilityToken, this);
+                _phone = Twilio.createDevice(token, dl);
+                Intent intent = new Intent();
+                intent.setAction("com.rogchap.react.modules.twilio.incoming");
+                PendingIntent pi = PendingIntent.getBroadcast(_reactContext, 0, intent, 0);
+                _phone.setIncomingIntent(pi);
               }
             } catch (Exception e) {
-              Log.e(e.getMessage(), e);
             }
           }
 
           @Override
           public void onError(Exception e) {
-            Log.e(e.getMessage(), e);
           }
       });
     }
@@ -83,9 +132,6 @@ public class TwilioModule extends ReactContextBaseJavaModule implements Connecti
 
   @ReactMethod
   public void connect(ReadableMap params) {
-    if (_phone != null) {
-      _connection = _phone.connect(params, this);
-    }
   }
 
   @ReactMethod
@@ -98,10 +144,6 @@ public class TwilioModule extends ReactContextBaseJavaModule implements Connecti
 
   @ReactMethod
   public void accept() {
-    _pendingConnection.accept();
-    _pendingConnection.setConnectionListener(this);
-    _connection = _pendingConnection;
-    _pendingConnection = null;
   }
 
   @ReactMethod
@@ -116,7 +158,7 @@ public class TwilioModule extends ReactContextBaseJavaModule implements Connecti
 
   @ReactMethod
   public void setMuted(Boolean isMuted) {
-    if (_connection && _connection.getState() == Connection.State.CONNECTED) {
+    if (_connection != null && _connection.getState() == Connection.State.CONNECTED) {
       _connection.setMuted(isMuted);
     }
   }
@@ -125,12 +167,12 @@ public class TwilioModule extends ReactContextBaseJavaModule implements Connecti
 
   @Override
   public void onConnecting(Connection connection) {
-    sendEvent("connectionDidStartConnecting", connection.getParameters());
+    sendEvent("connectionDidStartConnecting", null);
   }
 
   @Override
   public void onConnected(Connection connection) {
-    sendEvent("connectionDidConnect", connection.getParameters());
+    sendEvent("connectionDidConnect", null);
   }
 
   @Override
@@ -141,7 +183,7 @@ public class TwilioModule extends ReactContextBaseJavaModule implements Connecti
     if (connection == _pendingConnection) {
         _pendingConnection = null;
     }
-    sendEvent("connectionDidDisconnect", connection.getParameters());
+    sendEvent("connectionDidDisconnect", null);
   }
 
   @Override
@@ -149,5 +191,28 @@ public class TwilioModule extends ReactContextBaseJavaModule implements Connecti
     Map errors = new HashMap();
     errors.put("err", errorMessage);
     sendEvent("connectionDidFail", errors);
+  }
+
+  /* DeviceListener */
+  @Override
+  public void onStartListening(Device device) {
+    this.sendEvent("deviceDidStartListening", null);
+  }
+
+  @Override
+  public void onStopListening(Device device) {
+  }
+
+  @Override
+  public void onStopListening(Device inDevice, int inErrorCode, String inErrorMessage) {
+  }
+
+  @Override
+  public boolean receivePresenceEvents(Device device) {
+    return false;
+  }
+
+  @Override
+  public void onPresenceChanged(Device inDevice, PresenceEvent inPresenceEvent) {
   }
 }
